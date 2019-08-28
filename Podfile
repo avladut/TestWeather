c558546b91cca5f3d2b5c66e233715de33a0b989
27390 <>
2019-08-28 22:46:37 +0100

    added pods and basic classes

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TestWeather' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TestWeather
+pod 'RxSwift', '~> 5'
+pod 'RxCocoa', '~> 5'
+pod 'Alamofire'
+
+  target 'TestWeatherTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ TestWeather/User Interface/ViewControllers/LocationsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LocationsViewController.swift
+//  TestWeather
+//
+//  Created by Alex on 28/08/2019.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import UIKit
+
+class LocationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
